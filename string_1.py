02a461207d0e0f00f323441c79d4dfbda2ee9ba3
kinsiabdirahman <kinsiabdirahman@outlook.com>
2024-03-15 13:27:57 +0300

    Final change

--- string_1.py
+++ string_1.py
@@ -1,10 +1,17 @@
 def solution(S):
+    # Iterate through the strings that we have in the array
     for i in range(len(S)):
-        for j in range(i+1, len(S)):
+        for j in range(i + 1, len(S)):
+            # Check the position by iterating
             for k in range(len(S[i])):
+                #See if the current position is the same as the ind letter
                 if S[i][k] == S[j][k]:
+                 
                     return [i, j, k]
+    # return empty array if no similar letter is found
     return []
+
+
 
 # Tests
 print(solution(["abc", "bca", "dbe"]))  
